811ff35556393070287e8bda10cf5c58e4251e24
sonsurim <surim014@naver.com>
2023-09-16 00:35:59 +0900

    불필요한 주석 제거

--- node/vite.config.ts
+++ node/vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.js
 export default {
   server: {
     proxy: {
-      '/pokemon': 'http://localhost:3000', // Express 서버의 주소로 프록시
+      '/pokemon': 'http://localhost:3000'
     },
   },
 }
